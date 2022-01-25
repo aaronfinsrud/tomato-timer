04058c8737eda6129294378171a5aba4a6ffed1b
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-25 16:51:54 -0500

    rename index.jsx to App.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/App.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
