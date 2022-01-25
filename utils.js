1300eeb520d0a724af6548c47be564f50277986f
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-25 16:43:53 -0500

    initialize utils.js

---
+++ utils.js
@@ -0,0 +1,9 @@
+module.exports = {
+  minsToMs: (minutes) => minutes * 60000,
+  secsToMS: (seconds) => seconds * 1000,
+  MSToMinutesAndSeconds(millis) {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  },
+};
