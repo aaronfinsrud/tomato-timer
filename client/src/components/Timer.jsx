4c811e1f062e3e7b4ac06bd0ae588fcc72d2fedc
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-25 22:49:32 -0500

    refactor Timer component to be stateless

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -2,89 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import utils from '../../../utils';
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inBreak: false,
-      stopped: true,
-      timeRemaining: null,
-      intervalId: null,
-    };
-    this.toggleStopped = this.toggleStopped.bind(this);
-    this.updateTimeRemaining = this.updateTimeRemaining.bind(this);
-    this.toggleInBreak = this.toggleInBreak.bind(this);
-  }
-
-  componentDidMount() {
-    const { sessionLength } = this.props;
-    this.setState({
-      timeRemaining: sessionLength,
-    });
-  }
-
-  toggleStopped() {
-    const { stopped } = this.state;
-    let { intervalId } = this.state;
-    this.setState({ stopped: !stopped });
-    if (stopped) {
-      intervalId = setInterval(this.updateTimeRemaining, 1000);
-      this.setState({ intervalId });
-    } else {
-      clearInterval(intervalId);
-    }
-  }
-
-  toggleInBreak() {
-    const { inBreak } = this.state;
-    const { sessionLength, breakLength } = this.props;
-    const timeRemaining = inBreak ? sessionLength : breakLength;
-    this.setState({ inBreak: !inBreak, timeRemaining });
-    this.toggleStopped();
-  }
-
-  updateTimeRemaining() {
-    const { timeRemaining, intervalId } = this.state;
-    if (timeRemaining <= 0) {
-      this.toggleInBreak();
-      clearInterval(intervalId);
-    } else {
-      this.setState({ timeRemaining: timeRemaining - utils.secsToMS(1) });
-    }
-  }
-
-  render() {
-    const { inBreak, stopped, timeRemaining } = this.state;
-    return (
-      <div>
-        <div className="container">
-          <div id="tomato">
-            <div id="tomatoTop" />
-            <div id="tomatoFill" />
-            <div id="timer-container">
-              <div>
-                {inBreak ? 'Break Time!' : 'Work Time!'}
-              </div>
-              <div>
-                {utils.MSToMinutesAndSeconds(timeRemaining)}
-              </div>
-              <button
-                type="button"
-                onClick={this.toggleStopped}
-              >
-                {stopped ? 'Start' : 'Stop'}
-              </button>
+function Timer({
+  inBreak, isStopped, timeRemaining, toggleStopped,
+}) {
+  return (
+    <div>
+      <div className="container">
+        <div id="tomato">
+          <div id="tomatoTop" />
+          <div id="tomatoFill" />
+          <div id="timer-container">
+            <div>
+              {inBreak ? 'Break Time!' : 'Work Time!'}
             </div>
+            <div>
+              {utils.MSToMinutesAndSeconds(timeRemaining)}
+            </div>
+            <button
+              type="button"
+              onClick={toggleStopped}
+            >
+              {isStopped ? 'Start' : 'Stop'}
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Timer.propTypes = {
-  sessionLength: PropTypes.number.isRequired,
-  breakLength: PropTypes.number.isRequired,
+  inBreak: PropTypes.bool.isRequired,
+  isStopped: PropTypes.bool.isRequired,
+  timeRemaining: PropTypes.number.isRequired,
+  toggleStopped: PropTypes.func.isRequired,
 };
 
 export default Timer;
