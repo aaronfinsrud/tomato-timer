7adf7b8d1ecaca5c0b1eb7c63f9c859c380b73bd
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-25 20:24:14 -0500

    implement modal component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './App.css';
 import Timer from './components/Timer.jsx';
+import Modal from './components/Modal.jsx';
 import utils from '../../utils';
 
 class App extends React.Component {
@@ -10,15 +11,22 @@ class App extends React.Component {
     this.state = {
       sessionLength: utils.minsToMs(0.25),
       breakLength: utils.minsToMs(5),
+      settingsModalIsShowing: false,
     };
+    this.toggleSettingsModal = this.toggleSettingsModal.bind(this);
   }
 
   componentDidMount() {
 
   }
 
+  toggleSettingsModal() {
+    const { settingsModalIsShowing } = this.state;
+    this.setState({ settingsModalIsShowing: !settingsModalIsShowing });
+  }
+
   render() {
-    const { sessionLength, breakLength } = this.state;
+    const { sessionLength, breakLength, settingsModalIsShowing } = this.state;
     return (
       <div>
         <h1>Tomato clock</h1>
@@ -26,6 +34,14 @@ class App extends React.Component {
           sessionLength={sessionLength}
           breakLength={breakLength}
         />
+        <button type="button" onClick={this.toggleSettingsModal}>settings</button>
+        <Modal
+          isShowing={settingsModalIsShowing}
+          onClose={this.toggleSettingsModal}
+          title="Settings"
+        >
+          this is a test
+        </Modal>
       </div>
     );
   }
---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Modal = function ({
+  isShowing, title, onClose, children
+}) {
+  if (!isShowing) {
+    return null;
+  }
+  return (
+    <div className="modal-component">
+      <div className="modal-component-content">
+        <div className="modal-component-header">
+          <button type="button" onClick={onClose} className="close">&times;</button>
+          <h2>{title}</h2>
+        </div>
+        <div className="modal-component-body">
+          {children}
+        </div>
+        <div className="modal-component-footer" />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
